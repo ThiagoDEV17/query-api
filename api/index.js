a7ac9155bd865db408af4f3229df2f2745a0fed4
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2022-07-16 21:14:13 -0300

    Changed query api logs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mysql = require("mysql2");
+const moment = require("moment");
+require("colors");
 
-const connection = mysql.createPool("mysql://root:mariadbpw@localhost:49153/smash");
+const connection = mysql.createPool("mysql://root:mariadbpw@localhost:49153/pvp");
 
 const app = express();
 
@@ -10,13 +12,13 @@ app.use(express.json());
 app.post("/query", (req, res) => {
     const { query, params, resource } = req.body;
 
-    connection.query(query, params, (err, results) => {
-        if (err) {
-            res.status(500).send(err);
-            console.log("Filed to execute query -> " + err.message);
+    connection.query(query, params, (error, results) => {
+        if (error) {
+            res.status(500).send(error);
+            console.log("[" + moment().format("DD-MM-YYYY HH:MM") + "]" + "[" + resource + "]" + ":" + "Filed to execute query " + error.message.underline.red);
         } else {
             res.send(results);
-            console.log(`[${resource}]: Query ${query} with params ${params} was executed`);
+            console.log("[" + moment().format("DD-MM-YYYY HH:MM") + "]" + "[" + resource + "]" + ":" + "Query " + query.underline.green + " with params " + JSON.stringify(params).underline.green + " executed successfully");
         }
     });
 });
